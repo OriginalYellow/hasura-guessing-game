767caafa3fea7f82757e30570f6b3e190b035bb1
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-06-26 18:38:44 -0400

    delete about view; add game rules to home view

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomeView from '../views/Home.vue';
-import AboutView from '../views/About.vue';
 import PlayView from '../views/Play.vue';
 
 Vue.use(VueRouter);
@@ -11,11 +10,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HomeView,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView,
   },
   {
     path: '/play',
--- www/src/views/Home.vue
+++ www/src/views/Home.vue
@@ -5,6 +5,23 @@
         <!-- view title -->
         <p class="text-center text-h2">Home View</p>
       </v-col>
+
+      <v-col cols="6">
+        <p class="font-weight-bold">
+          Game Rules
+        </p>
+        <ul>
+          <li>when the game starts, the computer choses a random, secret number</li>
+          <li>in a set order, players take turns guessing the secret number</li>
+          <li>there are two possible ways to win:
+            <ul>
+              <li>if player X guesses correctly, they will instantly win</li>
+              <li>if player X's guess is the closest guess so far, and another player doesn't make a closer guess after player X makes theirs, player X will win at the start of their next turn</li>
+            </ul>
+          </li>
+          <li>the current closest guess (and the player who made it) will be displayed at all times to all players</li>
+        </ul>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -17,7 +34,7 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
+  name: "Home"
   // components: {
   //   HelloWorld,
   // },
