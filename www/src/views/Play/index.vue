529abf26dbbefb5ccbc43b720b7bd0e55647a766
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-06-29 15:23:30 -0400

    update play view to use reactive query variables

--- www/src/views/Play/index.vue
+++ www/src/views/Play/index.vue
@@ -48,14 +48,14 @@
 </template>
 
 <script>
-import { mapState } from "vuex"
-import * as RA from 'ramda-adjunct'
-import * as L from 'partial.lenses'
-import gameSessionByPk from "@/gql/gameSessionByPk.gql"
-import gameSessionByPkSubscription from "@/gql/gameSessionByPkSubscription.gql"
-import insertGameSessionUserOne from "@/gql/insertGameSessionUserOne.gql"
-import { Transform, Lens } from "@/lenses/gameSessionByPk.js"
-const { Data, Model, GameSessionByPk } = Lens
+import { mapState } from "vuex";
+import * as RA from "ramda-adjunct";
+import * as L from "partial.lenses";
+import gameSessionByPk from "@/gql/gameSessionByPk.gql";
+import gameSessionByPkSubscription from "@/gql/gameSessionByPkSubscription.gql";
+import insertGameSessionUserOne from "@/gql/insertGameSessionUserOne.gql";
+import { Transform, Lens } from "@/lenses/gameSessionByPk.js";
+const { Data, Model, GameSessionByPk } = Lens;
 
 // MIKE: you need to handle possible exceptions (like someone navigating
 // here without a query string)
@@ -63,6 +63,7 @@ export default {
   data() {
     return {
       gameSession: null,
+      debugId: 19
     };
   },
 
@@ -70,19 +71,21 @@ export default {
     gameSession() {
       return {
         query: gameSessionByPk,
-        variables: {
-          id: this.$route.query.id
+        variables() {
+          return {
+            id: this.$route.query.id
+          };
         },
         // NOTE: if there is no data payload in the response, update is passed
         // an empty object (for some dumb reason)
-        update: data => RA.isNilOrEmpty(data) ? null : Transform.data(data),
+        update: data => (RA.isNilOrEmpty(data) ? null : Transform.data(data)),
         subscribeToMore: {
           document: gameSessionByPkSubscription,
-          variables: {
-            id: this.$route.query.id
+          variables() {
+            return { id: this.$route.query.id };
           },
           updateQuery: (previousResult, { subscriptionData }) => {
-            return subscriptionData.game_session_by_pk
+            return subscriptionData.game_session_by_pk;
           }
         },
         result(response) {
@@ -90,12 +93,14 @@ export default {
             this.$apollo
               .mutate({
                 mutation: insertGameSessionUserOne,
-                variables: {
-                  gameSessionId: this.$route.query.id
-                }
+                variables: { gameSessionId: this.$route.query.id }
               })
               .then(
-                ({ data: { insert_game_session_user_one: { game_session } } }) => {
+                ({
+                  data: {
+                    insert_game_session_user_one: { game_session }
+                  }
+                }) => {
                   this.gameSession = game_session;
                 }
               );
@@ -116,8 +121,8 @@ export default {
       if (this.dataLoaded) {
         return this.gameSession.players.map(x => x.userName);
       }
-      
-      return []
+
+      return [];
     }
   },
 
